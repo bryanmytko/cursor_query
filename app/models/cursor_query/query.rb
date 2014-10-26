b2b0a3d754fc6f282a771dac5c6d389561832662
Bryan Mytko <bryanmytko@gmail.com>
2014-10-26 12:13:13 -0400

    Add Query model

--- app/models/cursor_query/query.rb
+++ app/models/cursor_query/query.rb
@@ -1,4 +1,8 @@
 module CursorQuery
   class Query < ActiveRecord::Base
+    def expired?
+      # TODO set expiration time as configuration
+      destroy! if DateTime.now > updated_at + 3.hours
+    end
   end
 end
